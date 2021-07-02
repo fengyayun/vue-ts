da7e89269ebf192f23c71872f112055026443ecb
fengyayun <941949529@qq.com>
2021-07-02 11:13:30 +0800

    调整初始目录

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/about">About</router-link>
-    </div>
+    <!-- 根路由的入口 -->
+    <h1>测试内容</h1>
     <router-view/>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style lang="scss" scoped>
 </style>
